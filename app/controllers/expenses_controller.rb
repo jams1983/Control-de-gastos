f64d6f3c7f5a9717c93764024530465701f18610
Jonathan Marin <jomarin_83@hotmail.com>
2011-11-11 11:39:53 -0500

    segundo commit

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,12 +3,47 @@ class ExpensesController < ApplicationController
     #@expenses = Expense.where(:service_id => params[:format])
     @service = Service.find(params[:service_id])
     @expenses= Expense.where(:service_id => @service.id).order(:year,:month)#.paginate(:per_page => 5, :page => params[:page])
+    first_expense=@expenses.first
+    last_expense=@expenses.last
+    
+    #obtiene los meses entre los años min y max de los expenses
+    num_meses=Array.new
+    if @expenses.size>0
+      if first_expense.year==last_expense.year
+        num_meses=(first_expense.month..last_expense.month).collect{|x| DateTime.new(first_expense.year,x,1)}
+      else
+        if last_expense.year-first_expense.year==1
+          num_meses=(first_expense.month..12).to_a.collect{|x| DateTime.new(first_expense.year,x,1)}
+          num_meses.concat((1..last_expense.month).to_a.collect{|x| DateTime.new(last_expense.year,x,1)})
+        else
+          num_meses=(first_expense.month..12).to_a.collect{|x| DateTime.new(first_expense.year,x,1)}
+          years=(first_expense.year+1...last_expense.year).to_a
+          years.each do |year|
+            num_meses.concat((1..12).to_a.collect{|x| DateTime.new(year,x,1)})
+          end
+          num_meses.concat((1..last_expense.month).to_a.collect{|x| DateTime.new(last_expense.year,x,1)})
+        
+        end
+      end
+      #obtiene los values de cada uno de los meses antes obtenidos, sino tiene value se le asigna nil
+      @vals=Array.new
+      num_meses.each do |nm|
+        exp = Expense.where(:service_id => @service.id).where(:year =>nm.year).where(:month =>nm.month)
+        if exp.size==0
+          @vals.push(nil)
+        else
+          @vals.push(exp[0].value)
+        end
+      end
+    end
+
+    #se obtienen los meses con formato mes/año para pasarlos a la vista
     @dates=Array.new
-    @expenses.each do |ex|
-      d=DateTime.new(ex.year,ex.month,1).strftime("%b/%y") 
+    num_meses.each do |nm|
+      d=nm.strftime("%b/%y") 
       @dates.push(d)
     end
-    puts "el valor",@dates
+
   end
   def new
     @service = Service.find(params[:service_id])
